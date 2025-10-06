0743e760ccac3b95a27cde9d1ef729baa499d7f3
JoMedeiros <gois.josivan@gmail.com>
2025-10-06 17:57:58 -0300

    correção do chrome driver

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 @pytest.fixture(scope="module")
 def browser():
     chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = "/usr/bin/chromedriver"
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     #chrome_options.add_argument("--no-sandbox")
--- tests/test_home.py
+++ tests/test_home.py
@@ -3,7 +3,7 @@ import time
 def test_home(browser):
     url = "http://127.0.0.1:8081"
     browser.get(url)
-    time.sleep(3)
+    time.sleep(2)
     assert browser.title == "Dash"
     print("Teste da página inicial concluído com sucesso.")
 
