4390e96e3ae9e93d21d987362f94c3f461961279
JoMedeiros <gois.josivan@gmail.com>
2025-10-06 18:09:20 -0300

    correção do chrome driver

--- conftest.py
+++ conftest.py
@@ -1,15 +1,15 @@
 import pytest
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture(scope="module")
 def browser():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = "/usr/bin/chromedriver"
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    #chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(options=chrome_options)
+    service = Service()
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     yield driver
     driver.quit()
