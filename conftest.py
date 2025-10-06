629e496e826c374c15a00551acce0dd39d650d74
JoMedeiros <gois.josivan@gmail.com>
2025-10-06 17:25:18 -0300

    Alterações para executar os testes

--- conftest.py
+++ conftest.py
@@ -4,10 +4,10 @@ from selenium import webdriver
 
 @pytest.fixture(scope="module")
 def browser():
-    chrome_path = "/home/josivan/Downloads/chrome-linux64/chrome"
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = chrome_path
-    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    #chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(options=chrome_options)
 
     yield driver
