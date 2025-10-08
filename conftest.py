af9cfa482aa5d67e5c74d51bec6893c133faad79
JoMedeiros <gois.josivan@gmail.com>
2025-10-08 10:35:12 -0300

    explicit driver path

--- conftest.py
+++ conftest.py
@@ -5,12 +5,12 @@ from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture(scope="module")
 def browser():
-    chrome_path = "/home/josivan/Downloads/chrome-linux64/chrome"
+    #chrome_path = "/home/josivan/Downloads/chrome-linux64/chrome"
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = chrome_path
+    #chrome_options.binary_location = chrome_path
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    service = Service()
+    service = Service("/usr/bin/chromedriver")
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     yield driver
