ab5794ad384f4df0e1943ccd30f6ceec6505adac
JoMedeiros <gois.josivan@gmail.com>
2025-10-08 16:27:11 -0300

    Criando venv do zero

--- conftest.py
+++ conftest.py
@@ -2,14 +2,12 @@
 import pytest
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture(scope="module")
 def browser():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(options=chrome_options)
+    options = webdriver.FirefoxOptions()
+    options.add_argument("-headless")
+    driver = webdriver.Firefox(options=options)
 
     yield driver
     driver.quit()
