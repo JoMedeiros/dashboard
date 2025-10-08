bab7c4a30fb9a64722b7d5d81bcc66efa9a04cdc
JoMedeiros <gois.josivan@gmail.com>
2025-10-08 17:04:55 -0300

    nova opção

--- conftest.py
+++ conftest.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import pytest
 import time
+
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 @pytest.fixture(scope="module")
 def browser():
-    options = webdriver.FirefoxOptions()
-    options.add_argument("-headless")
-    driver = webdriver.Firefox(options=options)
+    driver = webdriver.Chrome('/home/josivan/Downloads/ChromeDriver/chromedriver-linux64/chromedriver')
 
     yield driver
     driver.quit()
