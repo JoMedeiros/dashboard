145e457ffb1ee5c6a38496175b943ee6d2abe0b8
JoMedeiros <gois.josivan@gmail.com>
2025-10-08 15:06:58 -0300

    webdriver default conf

--- conftest.py
+++ conftest.py
@@ -6,13 +6,10 @@ from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture(scope="module")
 def browser():
-    #chrome_path = "/home/josivan/Downloads/chrome-linux64/chrome"
     chrome_options = webdriver.ChromeOptions()
-    #chrome_options.binary_location = chrome_path
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    service = Service("/usr/bin/chromedriver")
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
 
     yield driver
     driver.quit()
